d6a64b9db1f265686b9c03a5808d61490308a828
AlexeyGoncharenko <zomorc@gmail.com>
2024-04-22 15:10:14 +0200

    step 2. added twig, templates (layouts)

--- public/index.php
+++ public/index.php
@@ -1,15 +1,33 @@
 <?php
+
+use DI\Container;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$container = new Container();
+AppFactory::setContainer($container);
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+// Set view in container
+$container->set('view', function() {
+    return Twig::create(__DIR__ . '/../templates');
 });
+
+// Create app
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+$app->add(TwigMiddleware::createFromContainer($app));
+
+// Routes
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->get('/', function (Request $request, Response $response) use ($router) {
+    $params = [];
+    return $this->get('view')->render($response, 'index.twig', $params);
+})->setName('index');
 
 $app->run();
